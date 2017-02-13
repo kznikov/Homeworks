d811af42f0ea6b653ba7924058b0e73a28a38c44
Krasimir <Krasimir>
2017-02-14 00:31:25 +0200

    v2 better responsive

--- assets/javascript/problem_7.js
+++ assets/javascript/problem_7.js
@@ -51,24 +51,7 @@
 		}
 
 
-		var myIndex = 0;
-		carousel();
-
-		function carousel() {
-		    var i;
-		    var k;
-		    var x = document.getElementsByClassName("mySlides_2");
-		    var y = document.getElementsByClassName("posters");
-		    for (i = 0; i < x.length; i++) {
-		       x[i].style.display = "none";
-		       y[i].style.display = "none"; 
-		    }
-		    myIndex++;
-		    if (myIndex > x.length) {myIndex = 1}    
-		    x[myIndex-1].style.display = "block";
-		    y[myIndex-1].style.display = "block";
-		    setTimeout(carousel, 2000); // Change image every 2 seconds
-		}
+		
 
 
 		$(document).ready(function(){
